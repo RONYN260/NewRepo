bae5937923d9c4e16cbff94c1165f95f9e81cc2a
RONY <huancarony421@gmail.com>
2025-11-08 11:30:30 -0500

    agregando un chanchito felis

---
+++ chanchito.java
@@ -0,0 +1,14 @@
+public class chanchito {
+
+    public static void main(String[] args) {
+        System .out.println("chanchito feliz");
+        System .out.println("chanchito trsite");
+        System.out.println();
+    
+    }
+    class cuhanchitofelis{
+        String felis;
+        
+
+    }
+}
